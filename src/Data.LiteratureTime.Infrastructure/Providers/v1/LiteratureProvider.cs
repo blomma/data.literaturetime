365e12416971ee5ed937c48c88c88729ccc873d2
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-11-06 20:19:18 +0100

    Say goodbye to code that was not used, R.I.P.

--- src/Data.LiteratureTime.Infrastructure/Providers/v1/LiteratureProvider.cs
+++ src/Data.LiteratureTime.Infrastructure/Providers/v1/LiteratureProvider.cs
@@ -1,15 +1,9 @@
 namespace Data.LiteratureTime.Infrastructure.Providers.v1;
 
-using System.Collections.Generic;
 using Data.LiteratureTime.Core.Interfaces.v1;
 
 public class LiteratureProvider : ILiteratureProvider
 {
-    public List<string> GetLiteratureTimes()
-    {
-        return File.ReadAllLines("litclock_annotated.csv").ToList();
-    }
-
     public Task<string[]> GetLiteratureTimesAsync()
     {
         var relativePathToFile = GetPathToQuotes();
