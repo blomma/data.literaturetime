a7587bceb7ebfc506dab5c239dd2d4fc15e0f6c0
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-11-10 21:47:30 +0100

    Will upgrade to .net 7

--- src/Data.LiteratureTime.Core/StartupModules.cs
+++ src/Data.LiteratureTime.Core/StartupModules.cs
@@ -3,14 +3,22 @@ namespace Data.LiteratureTime.Core;
 using Irrbloss;
 using Irrbloss.Interfaces;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 public class StartupModule : IStartupModule
 {
     public void AddStartups(IEndpointRouteBuilder app)
     {
+        IConfiguration configuration = app.ServiceProvider.GetRequiredService<IConfiguration>();
+        var connectionString = configuration.GetConnectionString("Redis");
+
+        if (connectionString == null) {
+            throw new Exception();
+        }
+
         var redisConnection = app.ServiceProvider.GetRequiredService<RedisConnection>();
-        redisConnection.Initalize();
+        redisConnection.Initalize(connectionString);
 
         var literatureDataWorker2 =
             app.ServiceProvider.GetRequiredService<Workers.v2.LiteratureDataWorker>();
