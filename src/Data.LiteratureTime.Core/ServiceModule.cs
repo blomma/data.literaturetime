cc47eb1a5c0c731a3ccade58390b70f644dc6556
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-12-25 22:36:24 +0100

    Simplify init of redisconnect

--- src/Data.LiteratureTime.Core/ServiceModule.cs
+++ src/Data.LiteratureTime.Core/ServiceModule.cs
@@ -2,14 +2,25 @@ namespace Data.LiteratureTime.Core;
 
 using Irrbloss;
 using Irrbloss.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 public class ServiceModule : IServiceModule
 {
-    public void AddServices(IServiceCollection service)
+    public void AddServices(IServiceCollection service, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Redis");
+        if (connectionString == null)
+        {
+            throw new Exception();
+        }
+
         service.AddSingleton<Workers.v2.LiteratureDataWorker>();
-        service.AddSingleton<RedisConnection>();
+        service.AddSingleton(s =>
+        {
+            return new RedisConnection(connectionString);
+        });
+
         service.AddTransient<Interfaces.v2.ILiteratureService, Services.v2.LiteratureService>();
     }
 }
