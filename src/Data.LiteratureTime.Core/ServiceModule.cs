d19ec625eff78cb4b39662600aa056592967318d
Mikael Hultgren <blomma@artsoftheinsane.com>
2022-12-26 19:38:33 +0100

    Move redis service registration into Infrastructure

--- src/Data.LiteratureTime.Core/ServiceModule.cs
+++ src/Data.LiteratureTime.Core/ServiceModule.cs
@@ -3,23 +3,11 @@ namespace Data.LiteratureTime.Core;
 using Irrbloss.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using StackExchange.Redis;
 
 public class ServiceModule : IServiceModule
 {
     public void AddServices(IServiceCollection service, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("Redis");
-        if (connectionString == null)
-        {
-            throw new Exception();
-        }
-
-        service.AddSingleton<IConnectionMultiplexer>(c =>
-        {
-            return ConnectionMultiplexer.Connect(connectionString);
-        });
-
         service.AddSingleton<Workers.v2.LiteratureDataWorker>();
         service.AddTransient<Interfaces.v2.ILiteratureService, Services.v2.LiteratureService>();
     }
--- src/Data.LiteratureTime.Infrastructure/ServiceModule.cs
+++ src/Data.LiteratureTime.Infrastructure/ServiceModule.cs
@@ -3,11 +3,23 @@ namespace Data.LiteratureTime.Infrastructure;
 using Irrbloss.Interfaces;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
 
 public class ServiceModule : IServiceModule
 {
     public void AddServices(IServiceCollection service, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Redis");
+        if (connectionString == null)
+        {
+            throw new Exception();
+        }
+
+        service.AddSingleton<IConnectionMultiplexer>(c =>
+        {
+            return ConnectionMultiplexer.Connect(connectionString);
+        });
+
         service.AddTransient<
             Core.Interfaces.v2.ILiteratureProvider,
             Providers.v2.LiteratureProvider
