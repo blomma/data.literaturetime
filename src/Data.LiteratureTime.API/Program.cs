fff51e355e8e9114f9fbbcfeee90b7e72741175d
Mikael Hultgren <blomma@artsoftheinsane.com>
2023-01-16 11:21:23 +0100

    Convert to a worker service
    I had some grand idea that this worker would be able to recieve
    commands over http to control it's behaviour, but this is probably
    better handled via redis pub sub commands

--- src/Data.LiteratureTime.API/Program.cs
+++ src/Data.LiteratureTime.API/Program.cs
@@ -1,5 +1,4 @@
 using Irrbloss.Extensions;
-using Microsoft.AspNetCore.HttpLogging;
 using Serilog;
 using Serilog.Events;
 
@@ -9,51 +8,19 @@ Log.Logger = new LoggerConfiguration().MinimumLevel
     .WriteTo.Console()
     .CreateBootstrapLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Host.UseSerilog(
+var builder = Host.CreateDefaultBuilder(args);
+builder.UseSerilog(
     (context, services, configuration) =>
         configuration.ReadFrom.Configuration(context.Configuration).ReadFrom.Services(services)
 );
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddMvcCore();
+builder.ConfigureServices(
+    (context, services) =>
+    {
+        services.AddServiceModules(context.Configuration);
+        services.AddRouterModules();
+    }
+);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddServiceModules(builder.Configuration);
-builder.Services.AddRouterModules();
-
-builder.Services.AddManagedResponseException();
-
-builder.Services.AddHttpLogging(logging =>
-{
-    logging.RequestHeaders.Add("Referer");
-    logging.RequestHeaders.Add("X-Forwarded-For");
-    logging.RequestHeaders.Add("X-Forwarded-Host");
-    logging.RequestHeaders.Add("X-Forwarded-Port");
-    logging.RequestHeaders.Add("X-Forwarded-Proto");
-    logging.RequestHeaders.Add("X-Forwarded-Server");
-    logging.RequestHeaders.Add("X-Real-Ip");
-    logging.RequestHeaders.Add("Upgrade-Insecure-Requests");
-    logging.LoggingFields = HttpLoggingFields.All;
-    logging.RequestBodyLogLimit = 4096;
-    logging.ResponseBodyLogLimit = 4096;
-});
-
-var app = builder.Build();
-app.UseHttpLogging();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseManagedResponseException();
-app.UseRouterModules();
-
-app.Run();
+var host = builder.Build();
+host.Run();
