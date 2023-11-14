7861e28c1a17e8ea9fa2f528c176952cd745c608
Mikael Hultgren <blomma@artsoftheinsane.com>
2023-11-14 20:04:31 +0100

    .net 8itify

--- src/Data.LiteratureTime.Infrastructure/Providers/CacheProvider.cs
+++ src/Data.LiteratureTime.Infrastructure/Providers/CacheProvider.cs
@@ -4,14 +4,9 @@ using System.Text.Json;
 using Data.LiteratureTime.Core.Interfaces;
 using StackExchange.Redis;
 
-public class CacheProvider : ICacheProvider
+public class CacheProvider(IConnectionMultiplexer connectionMultiplexer) : ICacheProvider
 {
-    private readonly IConnectionMultiplexer _connectionMultiplexer;
-
-    public CacheProvider(IConnectionMultiplexer connectionMultiplexer)
-    {
-        _connectionMultiplexer = connectionMultiplexer;
-    }
+    private readonly IConnectionMultiplexer _connectionMultiplexer = connectionMultiplexer;
 
     public Task<bool> SetAsync<T>(string key, T data, TimeSpan? expiration = null)
     {
